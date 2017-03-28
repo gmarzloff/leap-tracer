cd1a24673e6f4596121bf9c07c9014bac6fae9b3
George Marzloff <george@marzloffmedia.com>
2017-03-28 17:39:19 -0400

    Spiral tracking complete

---
+++ part2/canvas.js
@@ -0,0 +1,181 @@
+// canvas.js
+// This javascript is used to manage the cursor tracer canvas 
+// Developed by George Marzloff (george@marzloffmedia.com)
+
+$( document ).ready(function() {
+  
+  	var spiral = new Spiral({				// we create a Spiral object here.
+		startPoint: {x: 400, y: 210},		// See Spiral() object in spiral.js
+		numberOfLoops: 3.15,
+		radiusGrowthRate: 0.15,
+		centerHoleRadius: 10				// adding a constant to the equation generates a center hole
+	}); 		
+
+	var hoverTargetsRadius = 20;			
+	var pathPoints = [];					// stores the path of the mouse
+	var deviations
+	var isTracking = false;					// flag to turn on/off tracking
+
+	// Setup an empty layer for userPath without any points yet
+	addUserPathLayer();
+	
+	// CREATE LAYER FOR SPIRAL GUIDELINE
+	$('canvas').drawLine({
+		strokeWidth: 3,
+		strokeStyle: '#aaa', // gray
+		visible: true,
+		name: 'guideline',
+		layer: true
+	});
+
+	// ADD GUIDELINE POINTS TO THE GUIDELINE LAYER
+	$('canvas').setLayer('guideline', spiral.guidelinePoints)
+	.drawLayers();
+	
+
+	// DRAW THE STARTING CIRCLE
+	$('canvas').drawArc({
+	  fillStyle: '#0a0', // green
+	  opacity: 0.75,
+	  x: spiral.start.x, y: spiral.start.y,
+	  radius: hoverTargetsRadius,
+	  layer: true,
+	  name: 'startCircle',
+	  mouseover: function() {
+	    $(this).animateLayer('startCircle', {
+	      fillStyle: '#0d0'
+	    }, 250);
+
+	  	isTracking = true;
+
+	  },
+	  mouseout: function() {
+	  	 $(this).animateLayer('startCircle', {
+	  	  	fillStyle: '#0a0'
+	  	}, 250);
+	  }
+	});
+
+	// DRAW THE TARGET CIRCLE
+	$('canvas').drawArc({
+	  fillStyle: '#00d', // green
+	  opacity: 0.75,
+	  x: spiral.endPoint.x, y: spiral.endPoint.y,
+	  radius: hoverTargetsRadius,
+	  layer: true,
+	  name: 'targetCircle',
+	  mouseover: function() {
+	    $(this).animateLayer('targetCircle', {
+	      fillStyle: '#55f'
+	    }, 250);
+	    
+	    if(isTracking){ 		// only trigger if we already hit the green circle
+		    isTracking = false;
+		    didHitFinish = true;
+		    analyzePerformance();
+		}
+	  },
+	  mouseout: function() {
+	  	 $(this).animateLayer('targetCircle', {
+	  	  	fillStyle: '#00d'
+	  	}, 250);
+	  }
+
+	});
+
+	// DRAW INSTRUCTIONS TEXT
+	$('canvas').drawText({
+	  fillStyle: '#000',
+	  x: 400, y: 20,
+	  fontSize: 14,
+	  fontFamily: 'Verdana, sans-serif',
+	  text: 'Mouseover the green circle and trace the spiral to the blue circle.',
+	  layer: true,
+	  name: 'instructionsText'
+	});
+
+	// DRAW RESET BUTTON
+	$('canvas').drawRect({
+		fillStyle: '#f00',
+		x: 60, y: 350,
+		width: 50, height: 40,
+		layer: true,
+		name: 'resetButton',
+		cornerRadius: 10,
+		click: function(){
+			resetPath();
+		}
+	});
+	$('canvas').drawText({
+		fillStyle: '#fff',
+		x: $('canvas').getLayer('resetButton').x,
+		y: $('canvas').getLayer('resetButton').y,
+		width: 50,
+		height: 40,
+		text: 'Reset',
+		layer: true,
+		name: 'resetText',
+		intangible: true
+	});
+
+	// INSTRUCTIONS TRIGGERED WHEN MOUSE MOVES
+  	$('canvas').mousemove(function(event){
+
+  		if(isTracking){ 	
+			// Create a path following the mouse
+			// add a point to the path array
+			var rect = $('canvas').offset();
+			// correct mouse coordinates
+			var cursor_x = event.pageX - rect.left;
+			var cursor_y = event.pageY - rect.top;
+
+			pathPoints.push([cursor_x, cursor_y]);	// add the cursor coordinates into an array
+
+			var i = pathPoints.length;	// use this # to create the property name e.g. x1, x2, x3, etc
+			var pathLayer = $('canvas').getLayer('userPath');	
+			pathLayer['x'+i] = pathPoints[i-1][0];
+			pathLayer['y'+i] = pathPoints[i-1][1];
+		}
+  	});
+
+	function addUserPathLayer(){
+		$('canvas').addLayer({
+			name: 'userPath',
+			type: 'line',
+			strokeStyle: '#f00',
+			strokeWidth: 3,
+			index: 4
+		});
+	}
+
+	function resetPath(){
+		// clear the points array, delete userPath and add a blank userPath layer
+	  	pathPoints = [];
+	  	isTracking = false;
+	  	$('canvas').removeLayer('userPath');
+	  	addUserPathLayer();
+	  	$('canvas').drawLayers();
+	}
+
+	function showGuideline(show){
+		$('canvas').setLayer('guideline', {visible: show}).drawLayers();
+	}
+
+	function analyzePerformance(){
+
+		// 1. calculate the total area of the deviations (aka the integral)
+		var areaUnderCurve = 0;
+		for(i=0; i<pathPoints.length; i++){
+			// a straight line parallel to the x-axis simplifies the math: 
+			// we just need to find the absolute difference in y pixels for every i-th x pixel.
+			var ydiff = pathPoints[i][1] - spiral.start.y; 
+			areaUnderCurve = areaUnderCurve + Math.abs(ydiff);
+
+			// Note: If you wanted this to work for a straight line no matter where the start/end points were,
+			// would need to calculate the slope of the line (y=mx+b) to identify the line's y value at each point
+		}
+
+		$('#results').html('[ Analysis in development ]');
+
+	}
+});
