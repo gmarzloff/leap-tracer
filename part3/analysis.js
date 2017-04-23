2128e38314df77bd6dbb007d1d36d15d290bfe75
George Marzloff <george@marzloffmedia.com>
2017-04-23 00:07:45 -0400

    changed analysis to ES5 version like part2

--- part3/analysis.js
+++ part3/analysis.js
@@ -2,37 +2,55 @@
 // Analysis of Spiral performance
 // Developed by George Marzloff (george@marzloffmedia.com)
 
-class Analysis {
+function Analysis(_userPath) {
 
 	// The r and theta polar coordinates of the spiral function are linearly related  
 	// So we can plot this relationship: the angles along the x-axis and r's on the y-axis.
 	// Since every change in cursor position should increase the radius in a linear fashion, we can 
 	// run a regression of (sample #, radius) points to calculate an accuracy score (using R^2).  
 
-	constructor(_userPath){
-		this.userPath = _userPath
-		this.radiiData = this.generateRadiiOnSamplesData(this.userPath);
-		this.regression = this.linearRegression(this.radiiData);
+	this.userPath = _userPath; 
 
-		var accuracyPct = Math.round(this.regression.r2 * 100); // rounds R^2
-		$('#results').html("Accuracy: " + accuracyPct + "%");
+	this.truncateInitialPointsBeforeTrueStart = function(data){
 
-		// Output data to CSV in the console with this function
-		// console.log(this.generateCSV(this.radiiData));
-	}
+		// Since recording starts when the cursor hits the border of the starting circle, 
+		// the radii start positive and decrease as user moves toward center, creating a dip in the data.
+		// we can find the minimum radius point near the start of the path and ignore the data points prior to that.
 
-	generateRadiiOnSamplesData(path){
+		var minRadius = 200; // picked a number definitely higher than the radius of the starting circle
+		var minElement = {index: 0, radius: minRadius};
+
+		var windowSizeToSearchForMinimum = 100; // will go through data indices 0 to this one.
+		if(data.length <= windowSizeToSearchForMinimum){ // make sure the path drawn is longer than the window
+			windowSizeToSearchForMinimum = data.length;
+		}
+
+		for(var j=0; j < windowSizeToSearchForMinimum; j++){
+			console.log(data[j]);
+			if (data[j].radius < minElement.radius){
+				minElement = {index: j, radius: data[j].radius}; // overwrites if the radius is lower than the prior min
+			}
+		}
+		data.splice(0, minElement.index-1); // this deletes everything before the minElement index.
+
+		return data;
+	};
+
+	this.generateRadiiOnSamplesData = function(path){
 		// incoming path is an array of objects of {x,y} points
 		var data = [];
 		for (var i=0; i<path.length; i++){
 			var pt = path[i];
 			var radius = Math.sqrt(pt.x*pt.x + pt.y*pt.y);
-			data.push({sample: i, radius: radius});
+			data.push({sample: i, radius: radius});		
 		}
+		this.truncateInitialPointsBeforeTrueStart(data);	
+		// see explanation in method above
+		
 		return data;
 	};
 
-	linearRegression(data){
+	this.linearRegression = function(data){
 		// Adapted from Trent Richardson's code snippet
 		// Credit: http://trentrichardson.com/2010/04/06/compute-linear-regressions-in-javascript/
 
@@ -45,28 +63,38 @@ class Analysis {
 		var sum_yy = 0;
 
 		for (var i = 0; i < data.length; i++) {
-			sum_x  += data[i].sample;
+			sum_x  += data[i].sample; // note the +=, it means sum_x = sum_x + data[i].sample.
 			sum_y  += data[i].radius;
 			sum_xy += (data[i].sample * data[i].radius);
 			sum_xx += (data[i].sample * data[i].sample);
 			sum_yy += (data[i].radius * data[i].radius);
 		} 
 
-		lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
-		lr['intercept'] = (sum_y - lr.slope * sum_x)/n;
-		lr['r2'] = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
-		lr['fn'] = function (x) { return this.slope * x + this.intercept; };
+		lr.slope = (n * sum_xy - sum_x * sum_y) / (n*sum_xx - sum_x * sum_x);
+		lr.intercept = (sum_y - lr.slope * sum_x)/n;
+		lr.r2 = Math.pow((n*sum_xy - sum_x*sum_y)/Math.sqrt((n*sum_xx-sum_x*sum_x)*(n*sum_yy-sum_y*sum_y)),2);
+		lr.fn = function (x) { return this.slope * x + this.intercept; };
 
 		return lr;
 	};
 
-	generateCSV(data){
+	this.radiiData = this.generateRadiiOnSamplesData(this.userPath);
+	this.regression = this.linearRegression(this.radiiData);
+
+	this.printResults = function(){
+		var accuracyPct = Math.round(this.regression.r2 * 100); // rounds R^2
+		$('#results').html("Accuracy: " + accuracyPct + "%");
+
+		// Output data to CSV in the console with this function
+		// console.log(this.generateCSV(this.radiiData));
+	};
+
+	this.generateCSV = function(data){
+		// utility function to output data in CSV format for adding to spreadsheet
 		var str = "";
 		for(var i=0; i< data.length; i++){
-			str += data[i].angle + ", " + data[i].radius + "\n";
+			str += i + ", " + data[i].radius + "\n";
 		}
-	}
+		return str;
+	};
 } 
-
-
-
